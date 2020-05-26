303b4c492bccc2b90157800d7a671d8323bf88ca
Satid Tachatanitanont <satidtachatanitanont@Satids-MacBook-Pro.local>
2020-05-27 03:17:06 +0700

    redux dev tool remove

--- src/redux/store.js
+++ src/redux/store.js
@@ -18,11 +18,6 @@ const store = createStore(
     reducers,
     initialState,
     applyMiddleware(...middleware)
-    // compose(
-    //     applyMiddleware(...middleware),
-    //     window.__REDUX_DEVTOOLS_EXTENSION__ &&
-    //         window.__REDUX_DEVTOOLS_EXTENSION__()
-    // )
 );
 
 export default store;
