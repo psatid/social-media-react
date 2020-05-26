6c973cf0161ad0e4befb923437ee8eb9426374ff
Satid Tachatanitanont <satidtachatanitanont@Satids-MacBook-Pro.local>
2020-05-27 03:09:42 +0700

    remove redux dev tool

--- src/redux/store.js
+++ src/redux/store.js
@@ -17,11 +17,12 @@ const reducers = combineReducers({
 const store = createStore(
     reducers,
     initialState,
-    compose(
-        applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ &&
-            window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
+    applyMiddleware(...middleware)
+    // compose(
+    //     applyMiddleware(...middleware),
+    //     window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    //         window.__REDUX_DEVTOOLS_EXTENSION__()
+    // )
 );
 
 export default store;
